20e144d1f27df1b7aa05ab9e1b39342284e36ecb
alingse <alingse@foxmail.com>
2023-05-11 21:29:24 +0800

    add nothing

--- slice.go
+++ slice.go
@@ -4,39 +4,16 @@ import (
 	"github.com/samber/lo"
 )
 
-type SliceChain[R any] []R
-type SliceChain2[T3, R any] []T3
-type SliceChain3[T2, T3, R any] []T2
-type SliceChain4[T1, T2, T3, T4 any] []T1
+type SliceChain[T1, T2, T3, T4 any] []T1
+type SliceChainComparable[T1 comparable, T2, T3, T4 any] SliceChain[T1, T2, T3, T4]
 
-type MapF[T, R any] func(T, int) R
-
-func (s SliceChain4[T1, T2, T3, R]) Map(f MapF[T1, T2]) SliceChain3[T2, T3, R] {
-	return lo.Map(s, f)
+func NewSliceChain4[T1, T2, T3, T4 any](s []T1) SliceChain[T1, T2, T3, T4] {
+	return s
 }
 
-func (s SliceChain3[T1, T2, R]) Map(f MapF[T1, T2]) SliceChain2[T2, R] {
-	return lo.Map(s, f)
+func (s SliceChain[T1, T2, T3, T4]) Map(f func(T1, int) T2) SliceChain[T2, T3, T4, any] {
+	return NewSliceChain4[T2, T3, T4, any](lo.Map(s, f))
 }
-
-func (s SliceChain2[T1, R]) Map(f MapF[T1, R]) SliceChain[R] {
-	return lo.Map(s, f)
-}
-
-type PredicateF[V any] func(V, int) bool
-
-func (s SliceChain4[T1, T2, T3, R]) Filter(predicate PredicateF[T1]) SliceChain4[T1, T2, T3, R] {
-	return lo.Filter(s, predicate)
-}
-
-func (s SliceChain3[T2, T3, R]) Filter(predicate PredicateF[T2]) SliceChain3[T2, T3, R] {
-	return lo.Filter(s, predicate)
-}
-
-func (s SliceChain2[T3, R]) Filter(predicate PredicateF[T3]) SliceChain2[T3, R] {
-	return lo.Filter(s, predicate)
-}
-func (s SliceChain[R]) Filter(predicate PredicateF[R]) SliceChain[R] { return lo.Filter(s, predicate) }
 
 /*
 func (s SliceChain4[T1, T2, T3, R]) Uniq() SliceChain4[T1, T2, T3, R] { return lo.Uniq(s) }
--- slice_test.go
+++ slice_test.go
@@ -4,7 +4,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,21 +12,31 @@ func TestSliceChain(t *testing.T) {
 		"the golang generic is ugly",
 		"how do you think?",
 	}
-
-	data := SliceChain3[string, []string, int](lines).
-		Filter(func(s string, i int) bool {
-			return len(s) > 10
-		}).
+	var x = NewSliceChain4[string, []string, int64, any](lines).
 		Map(func(s string, _ int) []string {
 			return strings.Fields(s)
 		}).
-		Map(func(s []string, i int) int {
-			return len(s)
-		}).
-		Filter(func(c, _ int) bool {
-			return c > 4
+		Map(func(s []string, i int) int64 {
+			return int64(len(s))
 		})
+	assert.Len(t, x, 2)
+	/*
+		var x = NewSliceChain3[string, []string, int](lines)
+		data := SliceChain3[string, []string, int](lines).
+			Filter(func(s string, i int) bool {
+				return len(s) > 10
+			}).
+			Map(func(s string, _ int) []string {
+				return strings.Fields(s)
+			}).
+			Map(func(s []string, i int) int {
+				return len(s)
+			}).
+			Filter(func(c, _ int) bool {
+				return c > 4
+			})
 
-	count := lo.Sum(data)
-	assert.Equal(t, count, 5)
+		count := lo.Sum(data)
+		assert.Equal(t, count, 5)
+	*/
 }
