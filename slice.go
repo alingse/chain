413898047c6806bf352f3fec67a3cece5eab3638
alingse <alingse@foxmail.com>
2023-05-11 09:16:43 +0800

    add slilly

--- slice.go
+++ slice.go
@@ -4,9 +4,9 @@ import (
 	"github.com/samber/lo"
 )
 
-type SliceChain[T1 any] SliceChain4[T1, any, any, any]
-type SliceChain2[T1, T2 any] SliceChain4[T1, T2, any, any]
-type SliceChain3[T1, T2, T3 any] SliceChain4[T1, T2, T3, any]
+type SliceChain[R any] []R
+type SliceChain2[T3, R any] []T3
+type SliceChain3[T2, T3, R any] []T2
 type SliceChain4[T1, T2, T3, T4 any] []T1
 
 type MapF[T, R any] func(T, int) R
@@ -22,3 +22,25 @@ func (s SliceChain3[T1, T2, R]) Map(f MapF[T1, T2]) SliceChain2[T2, R] {
 func (s SliceChain2[T1, R]) Map(f MapF[T1, R]) SliceChain[R] {
 	return lo.Map(s, f)
 }
+
+type PredicateF[V any] func(V, int) bool
+
+func (s SliceChain4[T1, T2, T3, R]) Filter(predicate PredicateF[T1]) SliceChain4[T1, T2, T3, R] {
+	return lo.Filter(s, predicate)
+}
+
+func (s SliceChain3[T2, T3, R]) Filter(predicate PredicateF[T2]) SliceChain3[T2, T3, R] {
+	return lo.Filter(s, predicate)
+}
+
+func (s SliceChain2[T3, R]) Filter(predicate PredicateF[T3]) SliceChain2[T3, R] {
+	return lo.Filter(s, predicate)
+}
+func (s SliceChain[R]) Filter(predicate PredicateF[R]) SliceChain[R] { return lo.Filter(s, predicate) }
+
+/*
+func (s SliceChain4[T1, T2, T3, R]) Uniq() SliceChain4[T1, T2, T3, R] { return lo.Uniq(s) }
+func (s SliceChain3[T2, T3, R]) Uniq() SliceChain3[T2, T3, R]         { return lo.Uniq(s) }
+func (s SliceChain2[T3, R]) Uniq() SliceChain2[T3, R]                 { return lo.Uniq(s) }
+func (s SliceChain[R]) Uniq() SliceChain[R]                           { return lo.Uniq(s) }
+*/
--- slice_test.go
+++ slice_test.go
@@ -9,19 +9,25 @@ import (
 )
 
 func TestSliceChain(t *testing.T) {
-	var x SliceChain3[string, []string, int] = []string{
+	var lines = []string{
 		"the golang generic is ugly",
 		"how do you think?",
 	}
 
-	data := x.
+	data := SliceChain3[string, []string, int](lines).
+		Filter(func(s string, i int) bool {
+			return len(s) > 10
+		}).
 		Map(func(s string, _ int) []string {
 			return strings.Fields(s)
 		}).
 		Map(func(s []string, i int) int {
 			return len(s)
+		}).
+		Filter(func(c, _ int) bool {
+			return c > 4
 		})
 
 	count := lo.Sum(data)
-	assert.Equal(t, count, 9)
+	assert.Equal(t, count, 5)
 }
